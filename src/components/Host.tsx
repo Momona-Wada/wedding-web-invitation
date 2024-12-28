d2f800096904bb7eaf4a1a919a05e6af09d79043
Momona-Wada <momona.9410@gmail.com>
2024-12-27 22:02:46 -0800

    Create Host components

---
+++ src/components/Host.tsx
@@ -0,0 +1,11 @@
+export const Host = () => {
+    return (
+        <>
+            <div className="relative h-[555px]">
+                <div className="absolute inset-0 
+                    bg-[url('/images/backgrounds/yellow_flower.jpg')] bg-cover">
+                </div>
+            </div>
+        </>
+    )
+}
