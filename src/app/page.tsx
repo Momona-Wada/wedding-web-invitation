e5718e3f465bf131e3785b1dc1ab1a1df7f40dc1
Momona-Wada <momona.9410@gmail.com>
2024-12-27 21:41:42 -0800

    Fix to display blue flowers all over the background

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,19 @@
+import { Host } from "@/components/Host";
 import { Message } from "@/components/Message";
 import { TopPhoto } from "@/components/TopPhoto";
 
 export default function Home() {
   return (
     <>
-    <div className="relative h-screen">
+    <div className="relative min-h-screen">
       <div className="absolute inset-0 
-        bg-[url('/images/backgrounds/light_blue_flower.jpg')] bg-cover bg-center h-screen opacity-20">
+        bg-[url('/images/backgrounds/light_blue_flower.jpg')] bg-center opacity-20">
       </div>
     
       <div className="relative">
         <TopPhoto />
         <Message />
+        <Host />
       </div>
     </div>
     </>
