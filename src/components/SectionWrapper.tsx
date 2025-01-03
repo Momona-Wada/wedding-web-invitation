f6544fe046ad97acd8053ba302ac5a40aa6da66d
Momona-Wada <momona.9410@gmail.com>
2025-01-03 00:39:29 -0800

    Update SectionWrapper

--- src/components/SectionWrapper.tsx
+++ src/components/SectionWrapper.tsx
@@ -9,10 +9,13 @@ type SectionWrapperProps = {
 export const SectionWrapper = ({background, title, children}: SectionWrapperProps) => {
     return (
         <>
-        <div className="relative h-[600px]">
+        <div className="relative min-h-screen]">
             <div 
                 className="absolute inset-0 bg-cover"
-                style={{backgroundImage: `url(/images/backgrounds/${background}.jpg)` }}
+                style={{
+                    backgroundImage: `url(/images/backgrounds/${background}.jpg)`,
+                    opacity: 0.8
+                }}
             >
             </div>
 
